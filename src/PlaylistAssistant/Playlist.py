02a2e4ce2b77e2afba35fc1ff84984f276e6414b
Camber6821 <vania.osinin@yandex.ru>
2020-06-10 01:09:54 +0300

    Fix and optimize playlist

--- src/PlaylistAssistant/Playlist.py
+++ src/PlaylistAssistant/Playlist.py
@@ -4,31 +4,41 @@ from MusicInfo import MusicInfo
 
 class Playlist:
 
-	def __init__(self, init_list = list(), loop = False):
+	def __init__(self, *, init_list = list(), loop = False):
 		self.record_list = [MusicInfo(el) for el in init_list]
 		self.position    = 0
 		self.loop        = bool(loop)
 
 
 	def next(self):
-		if self.loop or self.position < len(self.record_list):
-			self.position += 1
-			if self.position >= len(self.record_list):
-				self.position = 0
-			return self.record_list[self.position]
-		else: return self.getCurrent()
+		"""
+			Перематывает плейлист вперед и возвращает Playlist.getCurrent()
+			В случае ошибки возвращает None (см. Playlist.getCurrent)
+		"""
+		self.position += 1
+		if self.position >= self.getLength():
+			if self.loop: self.position = 0
+			else: self.position -= 1
+		return self.getCurrent()
 
 
 	def prev(self):
-		if self.loop or self.position > 0:
-			self.position -= 1
-			if self.position <= 0:
-				self.position = len(self.record_list) - 1
-			return self.record_list[self.position]
-		else: return self.getCurrent()
+		"""
+			Перематывает плейлист назад 
+			Возвращает Playlist.getCurrent()
+		"""
+		self.position -= 1
+		if self.position < 0:
+			if self.loop: self.position = self.getLength() - 1
+			else: self.position += 1
+		return self.getCurrent()
 
 
 	def add(self, mi):
+		"""
+			Если mi нет в списке, то добавляет его в конец
+			Возвращает успех операции (True/False)
+		"""
 		if not MusicInfo.isCorrect(mi): return False
 		if mi not in self.record_list:
 			self.record_list.append(mi)
@@ -37,59 +47,84 @@ class Playlist:
 
 
 	def delete(self, mi):
-		try:
-			pos = self.record_list.index(mi)
-			return self.deleteByPosition(pos)
-		except: return False
+		"""
+			Удаляет mi из списка
+			Возвращает успех операции (True/False)
+		"""
+		if not mi in self.record_list: return False
+		return self.deleteByPosition(self.record_list.index(mi))
 
 
 	def deleteByPosition(self, pos: int):
-		try:
-			pos = int(pos)
-			# если индекс несёт негативную энергию, то исправим это
-			if pos < 0: pos = self.getLength() + pos
-			if not (0 <= pos < self.getLength()): return False
-			del self.record_list[pos]
-			if pos <= self.position: self.position -= 1
-			return True
-		except: return False
+		"""
+			Удаляет элемент списка по индексу
+			Бросает TypeError, если isinstance(pos, int) == False
+			Возвращает успех операции (True/False)
+		"""
+		if not isinstance(pos, int): raise TypeError(f'Expected type \'int\', but \'{pos.__class__.__name__}\' was encountered')
+		if pos < 0: pos += self.getLength()
+		if not 0 <= pos < self.getLength(): return False
+		if pos <= self.position: self.position -= 1
+		del self.record_list[pos]
+		return True
 
 	def mix(self):
+		""" Перемешивает плейлист и сбрасывает позицию в 0 """
 		random.shuffle(self.record_list)
 		self.position = 0
 
 
-	def getLength(self): return len(self.record_list)
+	def getLength(self):
+		"""	Возвращает длину плейлиста """
+		return len(self.record_list)
 
 
-	def getPosition(self): return self.position
+	def getPosition(self):
+		"""	Возвращает текущую позицию в плейлисте """
+		return self.position
 
 
 	def setPosition(self, pos: int):
-		try:
-			pos = int(pos)
-			if pos < 0: pos = self.getLength() + pos
-			if not (0 <= pos < self.getLength()): return False
-			return True
-		except: return False
+		"""
+			Устанавливает текущую позицию в плейлисте
+			Можно использовать отрицательные индексы
+			Бросает TypeError, если isinstance(pos, int) == False
+			Возвращает успех операции (True/False)
+		"""
+		if not isinstance(pos, int): raise TypeError(f'Expected type \'int\', but \'{pos.__class__.__name__}\' was encountered')
+		if pos < 0: pos = self.getLength() + pos
+		if not (0 <= pos < self.getLength()): return False
+		return True
 
 
 	def getAll(self): return self.record_list
 
 
-	def getCurrent(self): return self.record_list[self.position]
+	def getCurrent(self):
+		"""
+			Возвращает MusicInfo текущего трека
+			Если плейлист пуст, то возвращает None
+		"""
+		if self.getLength == 0: return None
+		return self.record_list[self.position]
+		
 
 	def getByPosition(self, pos: int):
-		try:
-			pos = int(pos)
-			if pos < 0: pos = self.getLength() + pos
-			if not (0 <= pos < self.getLength()): return None
-			return self.record_list[pos]
-		except: return None
+		"""
+			Возвращает трек по индексу
+			Можно использовать отрицательные индексы
+			Бросает TypeError, если isinstance(pos, int) == False
+			Возвращает успех операции (True/False)
+		"""
+		if not isinstance(pos, int): raise TypeError(f'Expected type \'int\', but \'{pos.__class__.__name__}\' was encountered')
+		if pos < 0: pos = self.getLength() + pos
+		if not (0 <= pos < self.getLength()): return None
+		return self.record_list[pos]
+		
 
 	def isLoop(self): return self.loop
 
-
 	def setLoop(self, flag: bool): self.loop = bool(flag)
 
-	def isEnd(self): return self.position + 1 >= self.getLength()
+
+	def isEnd(self): return self.position == self.getLength() - 1
