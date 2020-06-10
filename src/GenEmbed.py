63509ee14bb4dad8b062f4e8f7c8bb4af3c6e213
Camber6821 <vania.osinin@yandex.ru>
2020-06-10 17:04:08 +0300

    Add functions to GenEmbed

--- src/GenEmbed.py
+++ src/GenEmbed.py
@@ -9,4 +9,29 @@ class EmbedTypes(enum.Enum):
 
 def get_embed(text, type = EmbedTypes.info):
 	""" Генерирует discord.Embed  """
-	return discord.Embed(description=type[1].format(text=text), colour=type[0])
+	color, msg = type
+	return discord.Embed(description=msg.format(text=text), colour=color)
+
+
+def get_info_embed(text):
+	""" Генерирует discord.Embed используя EmbedTypes.info """
+	color, msg = EmbedTypes.info
+	return discord.Embed(description=msg.format(text=text), colour=color)
+
+
+def get_ok_embed(text):
+	""" Генерирует discord.Embed используя EmbedTypes.ok """
+	color, msg = EmbedTypes.ok
+	return discord.Embed(description=msg.format(text=text), colour=color)
+
+
+def get_warning_embed(text):
+	""" Генерирует discord.Embed используя EmbedTypes.warning """
+	color, msg = EmbedTypes.warning
+	return discord.Embed(description=msg.format(text=text), colour=color)
+
+
+def get_error_embed(text):
+	""" Генерирует discord.Embed используя EmbedTypes.error """
+	color, msg = EmbedTypes.error
+	return discord.Embed(description=msg.format(text=text), colour=color)
