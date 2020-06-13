21085a140bdf18275fa5ac893b78dba1c5e33209
Chamber6821 <vania.osinin@yandex.ru>
2020-06-13 12:49:42 +0300

    Refactoring GenEmbed
    !!! NOT COMPATIBLE WITH CURRENT VERSION OF BOT !!!

--- src/GenEmbed.py
+++ src/GenEmbed.py
@@ -1,36 +1,42 @@
 import discord
 
-class EmbedTypes():
-	info    = (0x007D80, "{text}")
-	ok      = (0x006b12, ":white_check_mark: {text}")
-	warning = (0xadad00, ":warning: {text}")
-	error   = (0xa30000, ":anger: {text}")
 
-def get_embed(text, type = EmbedTypes.info):
-	""" Генерирует discord.Embed  """
-	color, msg = type
-	return discord.Embed(description=msg.format(text=text), colour=color)
+class MsgEmbed:
+	""" Генератор сообщений в Embed'ах """
 
-
-def get_info_embed(text):
-	""" Генерирует discord.Embed используя EmbedTypes.info """
-	color, msg = EmbedTypes.info
-	return discord.Embed(description=msg.format(text=text), colour=color)
+	class Types:
+		""" Типы Embed'ов """
+		info    = (0x007D80, "{msg}")
+		ok      = (0x006b12, ":white_check_mark: {msg}")
+		warning = (0xadad00, ":warning: {msg}")
+		error   = (0xa30000, ":anger: {msg}")
 
 
-def get_ok_embed(text):
-	""" Генерирует discord.Embed используя EmbedTypes.ok """
-	color, msg = EmbedTypes.ok
-	return discord.Embed(description=msg.format(text=text), colour=color)
+	def get(type: Types, msg: str) -> discord.Embed:
+		""" Возвращает Embed типа type с сообщением msg """
+		color, msg_template = type
+		return discord.Embed(description=msg_template.format(msg=msg), colour=color)
 
+	
+	def info(msg: str) -> discord.Embed:
+		""" Возвращает Embed типа info с сообщением msg """
+		color, msg_template = MsgEmbed.Types.info
+		return discord.Embed(description=msg_template.format(msg=msg), colour=color)
 
-def get_warning_embed(text):
-	""" Генерирует discord.Embed используя EmbedTypes.warning """
-	color, msg = EmbedTypes.warning
-	return discord.Embed(description=msg.format(text=text), colour=color)
+	
+	def ok(msg: str) -> discord.Embed:
+		""" Возвращает Embed типа ok с сообщением msg """
+		color, msg_template = MsgEmbed.Types.ok
+		return discord.Embed(description=msg_template.format(msg=msg), colour=color)
 
+	
+	def warning(msg: str) -> discord.Embed:
+		""" Возвращает Embed типа warning с сообщением msg """
+		color, msg_template = MsgEmbed.Types.warning
+		return discord.Embed(description=msg_template.format(msg=msg), colour=color)
 
-def get_error_embed(text):
-	""" Генерирует discord.Embed используя EmbedTypes.error """
-	color, msg = EmbedTypes.error
-	return discord.Embed(description=msg.format(text=text), colour=color)
+	
+	def error(msg: str) -> discord.Embed:
+		""" Возвращает Embed типа error с сообщением msg """
+		color, msg_template = MsgEmbed.Types.error
+		return discord.Embed(description=msg_template.format(msg=msg), colour=color)
