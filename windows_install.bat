3d5ba76737aefa346ceb8fb2ac859b7131d0473a
Chamber6821 <vania.osinin@yandex.ru>
2020-06-19 13:10:57 +0300

    bat file closer to ideal

--- windows_install.bat
+++ windows_install.bat
@@ -1,27 +1,141 @@
-@ECHO OFF
+@echo off
 
-SET logDIR=log
-SET envFILE=.env
+set LOG_DIR=log
+set ENV_FILE=.env
 
-ECHO please wait a moment... && ECHO.
+echo.
+echo       :::     ::: ::::::::::: ::::    :::    :::   ::::::::
+echo      :+:     :+:     :+:     :+:+:   :+:  :+:+:  :+:    :+:
+echo     +:+     +:+     +:+     :+:+:+  +:+    +:+         +:+ 
+echo    +#+     +:+     +#+     +#+ +:+ +#+    +#+       +#++:  
+echo    +#+   +#+      +#+     +#+  +#+#+#    +#+         +#+   
+echo    #+#+#+#       #+#     #+#   #+#+#    #+#  #+#    #+#    
+echo     ###     ########### ###    ####  #######  ########     
+echo.
+echo                  Start fucking batch
+echo.
 
-IF not exist %logDIR% (
-mkdir %logDIR%
-ECHO created ./%logDIR%/
-) ELSE ( ECHO ./%logDIR%/ already exists! )
+call :init ^
+&& call :createLogFolder ^
+&& call :createEnvFile ^
+&& call :installPipenv ^
+&& call :installDependences ^
+&& call :end
+del %STDERR%
+del EMPTY_FILE
 
-IF not exist %envFILE% (
-ECHO BOT_TOKEN=PlAcE-toKEn-HErE  >%envFILE%
-ECHO PREFIX=.                    >>%envFILE%
-ECHO created and wrote %envFILE%!
-) ELSE ( ECHO %envFILE% already exists! )
+echo.
+pause
 
-ECHO. && ECHO pip install pipenv && ECHO.
-pip install pipenv
 
-ECHO. && ECHO pipenv install && ECHO.
-pipenv install
+goto EOF
 
-ECHO. && ECHO   ^|^| && ECHO   ^|^| && ECHO   \/ && ECHO  DONE! (DO NOT FORGET to install ffmpeg if you have not done so already) && ECHO   /\ && ECHO   ^|^| && ECHO   ^|^| && ECHO.
+:init
+	set NAME="Batch: "
+	set NAME=%NAME:~1,-1%
+	set LOG_OUT=%~nx0.log
+	set STDERR=%~nx0.stderr
+	type nul > %LOG_OUT%
+	type nul > %STDERR%
+	call :setESC
+	echo %NAME%Init complete >> %LOG_OUT%
+exit /b 0
 
-PAUSE
+
+:end
+	echo %NAME%DONE >> %LOG_OUT%
+	echo %NAME%DONE!
+	echo %ESC%[33m%NAME%WARNING: Don't forget to install ffmpeg if it's not installed%ESC%[m
+exit /b 0
+
+
+:createLogFolder
+	if not exist %LOG_DIR% (
+		mkdir %LOG_DIR% >> %LOG_OUT% 2> %STDERR% ^
+		&& echo %NAME%Created log folder ^
+		|| (
+			echo %NAME%Error >> %LOG_OUT%
+			echo %ESC%[91m%NAME%Some kind of error while creating log folder :P%ESC%[m
+			echo %ESC%[91m%NAME%Installation log in %LOG_OUT%%ESC%[m
+		)
+	) else (
+		echo %NAME%Log folder already exists
+		echo %NAME%Log folder already exists >> %LOG_OUT%
+	)
+	call :writeStderr "function :createLogFolder"
+exit /b 0
+
+
+:createEnvFile
+	if not exist %ENV_FILE% (
+		(echo BOT_TOKEN=PlAcE-toKEn-HErE && echo PREFIX=.) > %ENV_FILE% 2> %STDERR% ^
+		&& echo %NAME%Created and init env file ^(please check this file^) ^
+		|| (
+			echo %NAME%Error >> %LOG_OUT%
+			echo %ESC%[91m%NAME%Some kind of error while creating env file :P%ESC%[m
+			echo %ESC%[91m%NAME%Installation log in %LOG_OUT%%ESC%[m
+		)
+	) else (
+		echo %NAME%Env file already exists ^(please check this file^)
+		echo %NAME%Env file already exists ^(please check this file^) >> %LOG_OUT%
+	)
+	call :writeStderr "function :createEnvFile"
+exit /b 0
+
+
+:installPipenv
+	echo %NAME%Installing pipenv using pip...
+	(echo %NAME%Installing pipenv using pip... && pip install pipenv) >> %LOG_OUT% 2> %STDERR% ^
+	|| (
+		call :writeStderr pip install pipenv
+		echo %NAME%Error code 1 >> %LOG_OUT%
+		echo %ESC%[91m%NAME%Some kind of error while installing pipenv :P%ESC%[m
+		echo %ESC%[91m%NAME%Installation log in %LOG_OUT%%ESC%[m
+		exit /b 1
+	)
+	call :writeStderr "pip install pipenv"
+exit /b 0
+
+
+:installDependences
+	echo %NAME%Installing dependencies using pipenv...
+	(echo %NAME%Installing dependencies using pipenv... && pipenv install) >> %LOG_OUT% 2> %STDERR% ^
+	|| (
+		call :writeStderr "pipenv install"
+		echo %NAME%Error code 2 >> %LOG_OUT%
+		echo %ESC%[91m%NAME%Some kind of error while installing dependencies :P%ESC%[m
+		echo %ESC%[91m%NAME%Installation log in %LOG_OUT%%ESC%[m
+		exit /b 2
+	)
+	call :writeStderr "pipenv install"
+exit /b 0
+
+
+:writeStderr
+	rem Дублирует STDERR в файл и если он не пустой, то и в консоль
+	rem Принимает один параметр в КАВЫЧКАХ
+	set author=%1
+	set author=%author:~1,-1%
+	if not exist EMPTY_FILE type nul > EMPTY_FILE
+	fc %STDERR% EMPTY_FILE > nul
+	set /a IS_NOT_EMPTY=%errorlevel%
+	if %IS_NOT_EMPTY% == 1 (
+		echo %NAME%STDERR from: %author%
+		type %STDERR%
+		echo %NAME%STDERR end
+		(
+			echo %NAME%STDERR from: %author%
+			type %STDERR%
+			echo %NAME%STDERR end
+		) >> %LOG_OUT%
+	)
+exit /b 0
+
+
+:setESC
+	rem Записывает код клавиши Escape в ESC
+	for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do rem"') do (
+	  set ESC=%%b
+	  exit /b 0
+	)
+exit /b 0
