b5b19cb7059232f748fc272a58ea70aa1ea2fddc
Chamber6821 <vania.osinin@yandex.ru>
2020-06-23 17:50:10 +0300

    fix bages and refactoring

--- src/Bot.py
+++ src/Bot.py
@@ -11,7 +11,7 @@ from PlaylistAssistant import *
 from Server import Server
 from GenEmbed import MsgEmbed
 
-
+############################################
 
 icons = {
 	'playlist' : 'https://img.icons8.com/pastel-glyph/FFFFFF/playlist.png',
@@ -24,7 +24,7 @@ servers = dict() # id : Server.Server
 client = commands.Bot(command_prefix=os.getenv('PREFIX'), case_insensitive=True)
 client.remove_command('help')
 
-
+############################################
 
 def get_server(ctx) -> Server:
 	"""
@@ -47,7 +47,39 @@ def get_mi_embed(mi):
 Server.generate_embed = get_mi_embed
 
 
+async def join_to_channel(ctx, channel):
+	"""
+		Подключает бота к каналу
+		return 0 - всё ок
+		return 1 - бот уже подключен к этому каналу
+		return 2 - боту не удалось подключится (см. логи)
+	"""
+	server_id = ctx.guild.id
+	logger.debug(f'Server: {server_id}. Joining to channel...')
 
+	# Мне накакать, что в питоне не принято юзать коды возврата
+	try:
+		server = get_server(ctx)
+		if server.voice is None:
+			server.voice = await channel.connect()
+			logger.debug(f'Server: {server_id}. Joined \'{channel}:{channel.position}\'')
+		elif server.voice.is_connected():
+			if server.voice.channel == channel:
+				logger.debug(f'Server: {server_id}. Join error: Already joined')
+				return 1
+			await server.voice.move_to(channel)
+			logger.debug(f'Server: {server_id}. Joined \'{channel}:{channel.position}\'')
+		else:
+			# Я не знаю как подключить уже существующий войс, если это вообще возможно -_-
+			server.voice = await channel.connect()
+			logger.debug(f'Server: {server_id}. Joined \'{channel}:{channel.position}\'')
+	except:
+		logger.exception(f'Server: {server_id}. Join error: unknown')
+		return 2
+	return 0
+
+
+############################################
 
 @client.event
 async def on_ready():
@@ -67,12 +99,10 @@ async def on_error(event, ctx, *args, **kwargs):
 	await ctx.send(embed=MsgEmbed.error('!!! ОШИБКА !!!'))
 
 @client.event
-
 async def on_command(ctx):
 	server_id = ctx.guild.id
 	logger.debug(f'Server: {server_id}. Command: {ctx.command} {ctx.args[1:]}')
 	
-
 
 @client.event
 async def on_command_error(ctx, error):
@@ -85,7 +115,7 @@ async def on_command_error(ctx, error):
 	else:
 		raise error
 
-
+############################################
 
 @client.command(name='help')
 async def _help(ctx, cmd=''):
@@ -100,26 +130,28 @@ async def _help(ctx, cmd=''):
 
 
 @client.command()
-async def join(ctx, channel_name = '', *, from_user = True):
+async def join(ctx, channel_name = '', *, reconnect = True):
 	server_id = ctx.guild.id
-	if from_user: logger.debug(f'Server: {server_id}. Joining...')
-	else: logger.debug(f'Server: {server_id}. Auto joining...')
+	
+	if not reconnect:
+		voice = get_server(ctx).voice
+		if voice and voice.is_connected(): return True
 
 	# получаем канал для подключения (или вылетаем с ошибкой)
 	if channel_name == '':
-		try:
-			channel = ctx.author.voice.channel
-		except AttributeError:
+		if ctx.author.voice is None:
 			logger.debug(f'Server: {server_id}. Join error: User is not connected')
 			await ctx.send(embed=MsgEmbed.error('Присоединись к каналу, мудак'))
 			return False
+		channel = ctx.author.voice.channel
 	else:
 		channels = list(filter(lambda x: x.name==channel_name, ctx.guild.voice_channels))
 		if len(channels) == 0:
 			logger.debug(f'Server: {server_id}. Join error: Channel not found')
 			await ctx.send(embed=MsgEmbed.error('Ты инвалид? Название канала неправильное'))
 			return False
-		elif len(channels) > 1:
+		elif len(channels) == 1: channel = channels[0]
+		else:
 			# составляем список каналов и отправляем его
 			_list = [f'{i + 1} :  {x} ({x.position + 1}-й)\n' for i,x in enumerate(channels)]
 			emb = MsgEmbed.info(''.join(['0. Отмена\n'] + _list))
@@ -149,33 +181,16 @@ async def join(ctx, channel_name = '', *, from_user = True):
 				logger.debug(f'Server: {server_id}. Join error: User answer timeout')
 				await ctx.send(embed=MsgEmbed.warning('Время вышло. Подключение отменено'))
 				return False
-		else:
-			channel = channels[0]
+			
 
-	server = get_server(ctx)
+	result = await join_to_channel(ctx, channel)
+	if result == 0: await ctx.send(embed=MsgEmbed.ok(f'Бот подключился к каналу: {channel}'))
+	elif result == 1: await ctx.send(embed=MsgEmbed.warning(f'Бот уже подключен к каналу: {channel}'))
+	elif result == 2: await ctx.send(embed=MsgEmbed.error(f'Ты че наделал?'))
+	else: await ctx.send(embed=MsgEmbed.error('Ничего не понял, но очень интересно'))
 
-
-	if server.voice is None:
-		server.voice = await channel.connect()
-		logger.debug(f'Server: {server_id}. Joined \'{channel}:{channel.position}\'')
-		if from_user:
-			await ctx.send(embed=MsgEmbed.ok(f'Бот подключился к каналу: {channel}'))
-	elif server.voice.is_connected():
-		if server.voice.channel == channel:
-			logger.debug(f'Server: {server_id}. Join error: Already joined')
-			if from_user:
-				await ctx.send(embed=MsgEmbed.warning(f'Бот уже подключен к каналу: {channel}'))
-			return True
-		await server.voice.move_to(channel)
-		logger.debug(f'Server: {server_id}. Joined \'{channel}:{channel.position}\'')
-		await ctx.send(embed=MsgEmbed.ok(f'Бот подключился к каналу: {channel}'))
-	else:
-		server.voice = await channel.connect()
-		logger.debug(f'Server: {server_id}. Joined \'{channel}:{channel.position}\'')
-		if from_user:
-			await ctx.send(embed=MsgEmbed.ok(f'Бот подключился к каналу: {channel}'))
-
-	return True
+	# Я не хачу пихать return в if'ы, что бы не нарушать гармонию форматирования
+	return 0 <= result <= 1
 
 
 @client.command()
@@ -190,19 +205,14 @@ async def leave(ctx):
 
 @client.command()
 async def play(ctx, *args):
-	if not await join(ctx, from_user=False):
-		await ctx.send(embed=MsgEmbed.error('Задача успешно провалена'))
-		return
-	if len(args) > 0:
-		await add(ctx, *args)
-	get_server(ctx).start()
+	if len(args) > 0: await add(ctx, *args)
+	if await join(reconnect = False): get_server(ctx).start()
 
 
 @client.command()
 async def pause(ctx):
-	server_id = ctx.guild.id
-	if server_id in servers:
-		servers[server_id].voice.pause()
+	voice = get_server(ctx).voice
+	if voice: voice.pause()
 
 
 @client.command()
@@ -222,19 +232,20 @@ async def skip(ctx, count: int = 1):
 
 @client.command()
 async def add(ctx, *args):
-	server_id = ctx.guild.id
 	server = get_server(ctx)
 
 	search_query = ' '.join(args)
-	if search_query == '': return
+	if search_query == '':
+		await ctx.send(embed=MsgEmbed.warning('Хватит зря расходовать ресурсы бота'))
+		return
 
 	mi_list = musicSearch('vk', search_query, 5)
 	if mi_list is None: await ctx.send(embed=MsgEmbed.error('Ошибка поиска!')); return
 	if mi_list == []: await ctx.send(embed=MsgEmbed.warning('Ничего не найдено!')); return
 
 	# генерируем и отправляем список треков
-	_list = [f'{i + 1} :  {mi.artist} - {mi.title}\n' for i,mi in enumerate(mi_list)]
-	emb = MsgEmbed.info(''.join(['0. Отмена\n'] + _list))
+	_list = [f'{i + 1} :  {mi.artist} - {mi.title}' for i,mi in enumerate(mi_list)]
+	emb = MsgEmbed.info('\n'.join(['0. Отмена'] + _list))
 	emb.set_author(name = 'выберите песню', icon_url = icons['search'])
 	await ctx.send(embed = emb)
 
@@ -245,21 +256,14 @@ async def add(ctx, *args):
 				return message.author == author and message.content.isdigit()
 			return is_message_correct
 
-		while True:
-			logger.debug(f'Server: {server_id}. Wait message from author: {ctx.author}')
-			msg = await client.wait_for('message', check=get_message_check(ctx.author), timeout=10)
-			text = msg.content
-
-			try:
-				index = int(text)
-				if index == 0:
-					logger.debug(f'Server: {server_id}. Canceled')
-					await ctx.send(embed=MsgEmbed.warning('Отменено'))
-					return
-				mi = mi_list[index - 1]
-				logger.debug(f'Server: {server_id}. Received message from author: {ctx.author}')
-				break
-			except IndexError: await ctx.send(embed=MsgEmbed.error('Ты кто такой, сука, чтоб это делать?'))
+		msg = await client.wait_for('message', check=get_message_check(ctx.author), timeout=10)
+		index = int(msg.content)
+		if index == 0:
+			await ctx.send(embed=MsgEmbed.warning('Отменено'))
+			return
+		index -= 1
+		if 0 <= index < len(mi_list): mi = mi_list[index]
+		else: await ctx.send(embed=MsgEmbed.error('Ты кто такой, сука, чтоб это делать?')); return
 	except asyncio.exceptions.TimeoutError:
 		await ctx.send(embed=MsgEmbed.warning('Кто не успел - тот опоздал'))
 		return
@@ -271,46 +275,35 @@ async def add(ctx, *args):
 
 @client.command()
 async def remove(ctx, arg):
-	server_id = ctx.guild.id
-	if not server_id in servers:
-		logger.debug(f'Server: {server_id}. Remove error: No server')
-		await ctx.send(embed=MsgEmbed.warning('Туз не на месте'))
-		return
-	server = servers[server_id]
+	server = get_server(ctx)
 
 	if arg == 'all':
 		server.playlist.deleteAll()
-		server.voice.stop()
+		if server.voice: server.voice.stop()
 		await ctx.send(embed=MsgEmbed.info('Удалил весь плейлист :fire:'))
 		return
-	if not arg.isdigit():
-		raise commands.MissingRequiredArgument()
-	pos = int(arg)
+	
+	if not arg.isdigit(): raise commands.MissingRequiredArgument()
+	else: pos = int(arg)
 
 	if not 0 <= pos < server.playlist.getLength():
-		logger.debug(f'Server: {server_id}. Remove error: Invalid position: {pos}')
 		await ctx.send(embed=MsgEmbed.error('Как может в казино быть колода разложена в другом порядке?! Ты чё, бредишь, что ли?! Ёбаный твой рот, а!..'))
 		return
 	
 	mi = server.playlist.getByPosition(pos)
-	old_pos = server.playlist.getPosition()
-	if server.playlist.deleteByPosition(pos):
-		await ctx.send(embed=MsgEmbed.info(f'Удалил: {mi.artist} - {mi.title} :fire:'))
-		if old_pos == pos and server.voice is not None: server.voice.stop()
-	else:
-		await ctx.send(embed=MsgEmbed.error('Ошибка удаления'))
+	playlist, voice = server.playlist, server.voice
+	if pos == playlist.getPosition() and voice: voice.stop()
+	if playlist.deleteByPosition(pos): await ctx.send(embed=MsgEmbed.info(f'Удалил: {mi.artist} - {mi.title} :fire:'))
+	else: await ctx.send(embed=MsgEmbed.error('Ошибка удаления'))
 
 
 @client.command()
 async def playing(ctx):
-	server_id = ctx.message.guild.id
-	if not server_id in servers: await ctx.send(embed=MsgEmbed.error('А у вас плейлист не создан')); return
-	server = servers[server_id]
-	mi = server.playlist.getCurrent()
-	if mi is None:
-		await ctx.send(embed=MsgEmbed.info('Если долго всматриваться в пустоту, то ты увидишь этот плейлист'))
-	else:
-		await ctx.send(embed=get_mi_embed(mi))
+	playlist = get_server(ctx).playlist
+	if playlist.getLength() == 0:
+		await ctx.send(embed=MsgEmbed.warning('Плейлист пуст'))
+		return
+	await ctx.send(embed=get_mi_embed(playlist.getCurrent()))
 
 
 @client.command()
@@ -321,11 +314,10 @@ async def queue(ctx):
 		emb = MsgEmbed.info('Плейлист пуст')
 	else:
 		mi_list = server.playlist.getAll()
-		_list = [f'{i} :  {mi.artist} - {mi.title}\n' for i,mi in enumerate(mi_list)]
+		_list = [f'{i} :  {mi.artist} - {mi.title}' for i,mi in enumerate(mi_list)]
 		pos = server.playlist.getPosition()
-		mi = mi_list[pos]
-		_list[pos] = f'**{pos} :  {mi.artist} - {mi.title}**\n'
-		emb = MsgEmbed.info(''.join(_list))
+		_list[pos] = f'**{_list[pos]}**'
+		emb = MsgEmbed.info('\n'.join(_list))
 
 	emb.set_author(name = 'текущий плейлист', icon_url = icons['playlist'])
 	await ctx.send(embed = emb)
