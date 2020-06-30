883d81addcbbc7f2485d3302715a31d29df17f68
momscoder <dimagfenics@gmail.com>
2020-06-30 15:12:59 +0300

    my shit-changes for logging and exit

--- src/main.py
+++ src/main.py
@@ -1,5 +1,14 @@
 import logging
 import logging.config
+
+#region shit
+logging.basicConfig(filename="log/main.log", format='%(message)s', level=logging.DEBUG)
+logging.debug('####################\n')
+logging.root.removeHandler(logging.root.handlers[0])
+logging.basicConfig(filename="log/debug.log", format='%(message)s', level=logging.DEBUG)
+logging.debug('####################\n')
+#endregion
+
 logging.config.fileConfig('conf/logging.conf')
 logger = logging.getLogger(__name__)
 # надо разобраться с этими логерами (ну так разберись)
@@ -8,15 +17,22 @@ logging.getLogger('discord').setLevel(logging.WARNING)
 logging.getLogger('websockets').setLevel(logging.WARNING)
 logging.getLogger('asyncio').setLevel(logging.WARNING)
 
-import os
+import sys, os, signal
 
 import Bot
 import HelpLoader
 import AboutLoader
 
 def main():
-	try:	
-		logger.info('####################') #ыыы
+	def exit(sighnum = None, frame = None):
+		logger.info('Stop playing threads...')
+		servers = Bot.servers.values()
+		for server in servers: server.stop()
+		for server in servers: server.playing_thread.join()
+		logger.info('Program end.\n')
+		sys.exit(0)
+	signal.signal(signal.SIGHUP, exit)
+	try:
 		logger.info('Program start')
 		prefix = os.getenv('PREFIX')
 		Bot.help_docs = HelpLoader.load_help_docs('conf/help.json', prefix)
@@ -27,10 +43,6 @@ def main():
 	except Exception as error:
 		logger.exception(error)
 	finally:
-		logger.info('Stop playing threads...')
-		servers = Bot.servers.values()
-		for server in servers: server.stop()
-		for server in servers: server.playing_thread.join()
-		logger.info('Program end.')
+		exit()
 
 if __name__ == '__main__': main()
