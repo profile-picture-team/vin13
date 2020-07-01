75c108dccc28464c142182c78a0bb8a43376c29f
momscoder <dimagfenics@gmail.com>
2020-07-01 14:37:15 +0300

    make normal loop closing

--- src/main.py
+++ src/main.py
@@ -18,18 +18,29 @@ logging.getLogger('discord').setLevel(logging.WARNING)
 logging.getLogger('websockets').setLevel(logging.WARNING)
 logging.getLogger('asyncio').setLevel(logging.WARNING)
 
-import sys, os, platform, atexit # я знаю, что можно использвать sys, но нихачу
+import sys, os, atexit, asyncio, platform
+loop = asyncio.get_event_loop()
 
 import Bot
 import HelpLoader
 import AboutLoader
 
 def main():
+	
 	def on_exit():
+		logger.info('Shutdown bot')
+		loop.run_until_complete(Bot.client.logout())
+		
 		logger.info('Stop playing threads...')
 		servers = Bot.servers.values()
 		for server in servers: server.stop()
 		for server in servers: server.playing_thread.join()
+
+		for task in asyncio.Task.all_tasks():
+			logger.info(f'Cancel {task.get_coro()}')
+			task.cancel()
+		loop.stop()
+
 		logger.info('Program end.')
 		sys.exit(0)
 
@@ -38,21 +49,23 @@ def main():
 		sys.exit(0)
 
 	atexit.register(on_exit)
-	if platform.system() != 'Windows': 
+	if platform.system() != 'Windows': # я знаю, что можно использвать sys, но нихачу
 		import signal
 		signal.signal(signal.SIGHUP, on_terminate)
+		logger.warning('It is recommended to stop the program in ^C')
 	else: 
-		logger.warning('Stop program at ^C')
+		logger.warning('Stop program in ^C')
 
 	try:
 		logger.info('Program start')
 		prefix = os.getenv('PREFIX')
 		Bot.help_docs = HelpLoader.load_help_docs('conf/help.json', prefix)
 		Bot.about_docs = AboutLoader.load_about_docs('conf/about.json', prefix)
-		Bot.client.run(os.getenv('BOT_TOKEN'))
+		logger.info('Bot starts and preparing to work')
+		loop.run_until_complete(Bot.client.start(os.getenv('BOT_TOKEN')))
 	except KeyboardInterrupt:
 		logger.warning('Interrupted')
-		on_exit()
+		sys.exit(0)
 	except Exception as error:
 		logger.exception(error)
 
