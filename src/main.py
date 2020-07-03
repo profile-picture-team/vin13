55535c1f035db4f432a2bf7a6680d63d9573ff9c
momscoder <dimagfenics@gmail.com>
2020-07-03 19:45:38 +0300

    previous loop closing was not so normal

--- src/main.py
+++ src/main.py
@@ -1,60 +1,51 @@
 import logging
 import logging.config
 
-#region shit
-logging.basicConfig(filename="log/main.log", format='%(message)s', level=logging.DEBUG)
-logging.debug('')
-logging.root.removeHandler(logging.root.handlers[0])
-logging.basicConfig(filename="log/debug.log", format='%(message)s', level=logging.DEBUG)
-logging.debug('')
-#endregion
-
 logging.config.fileConfig('conf/logging.conf')
 logger = logging.getLogger(__name__)
-# надо разобраться с этими логерами (ну так разберись)
-# лол, а я знаю как?
 logging.getLogger('urllib3').setLevel(logging.WARNING)
 logging.getLogger('discord').setLevel(logging.WARNING)
 logging.getLogger('websockets').setLevel(logging.WARNING)
 logging.getLogger('asyncio').setLevel(logging.WARNING)
 
-import sys, os, atexit, asyncio, platform
-loop = asyncio.get_event_loop()
+import sys, os, platform
 
 import Bot
 import HelpLoader
 import AboutLoader
 
-def main():
-	
-	def on_exit():
+def run_once(f):
+    def wrapper(*args, **kwargs):
+        if not wrapper.has_run:
+            wrapper.has_run = True
+            return f(*args, **kwargs)
+    wrapper.has_run = False
+    return wrapper
+
+def stop_playing_threads(servers):
+	logger.info('Stop playing threads...')
+	for server in servers: server.stop()
+	for server in servers: server.playing_thread.join()
+
+@run_once
+def exit():
+	try:
 		logger.info('Shutdown bot')
-		loop.run_until_complete(Bot.client.logout())
-		
-		logger.info('Stop playing threads...')
-		servers = Bot.servers.values()
-		for server in servers: server.stop()
-		for server in servers: server.playing_thread.join()
-
-		for task in asyncio.Task.all_tasks():
-			logger.info(f'Cancel {task.get_coro()}')
-			task.cancel()
-		loop.stop()
-
-		logger.info('Program end.')
+		stop_playing_threads(Bot.servers.values())
+	except Exception as err:
+		logger.exception(f'Error while exiting: {err}')
+		logger.error('Program ended with error!\n')
+		sys.exit(1)
+	else:
+		logger.info('Program end.\n')
 		sys.exit(0)
 
-	def on_terminate(sighnum, frame):
-		logger.warning('Non ^C closing!')
-		sys.exit(0)
-
-	atexit.register(on_exit)
-	if platform.system() != 'Windows': # я знаю, что можно использвать sys, но нихачу
-		import signal
-		signal.signal(signal.SIGHUP, on_terminate)
-		logger.warning('It is recommended to stop the program in ^C')
-	else: 
-		logger.warning('Stop program in ^C')
+def main():
+	if platform.system() != 'Windows': # я знаю, что можно использовать sys, но нихачу
+		def on_terminate(sighnum, frame): exit()
+		import signal; signal.signal(signal.SIGHUP, on_terminate)
+		print('Achtung: It is recommended to stop program in ^C')
+	else: print('Achtung: Stop program in ^C')
 
 	try:
 		logger.info('Program start')
@@ -62,11 +53,9 @@ def main():
 		Bot.help_docs = HelpLoader.load_help_docs('conf/help.json', prefix)
 		Bot.about_docs = AboutLoader.load_about_docs('conf/about.json', prefix)
 		logger.info('Bot starts and preparing to work')
-		loop.run_until_complete(Bot.client.start(os.getenv('BOT_TOKEN')))
-	except KeyboardInterrupt:
-		logger.warning('Interrupted')
-		sys.exit(0)
-	except Exception as error:
-		logger.exception(error)
+		Bot.client.run(os.getenv('BOT_TOKEN'))
+	except KeyboardInterrupt: logger.warning('Interrupted'); exit()
+	except Exception as error: logger.exception(error)
+	finally: exit()
 
 if __name__ == '__main__': main()
